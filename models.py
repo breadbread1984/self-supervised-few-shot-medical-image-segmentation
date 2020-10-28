5e23c057779f87121fb1c93ef3fe6ef903733d10
breadbread1984 <breadbread1984@163.com>
2020-10-28 11:37:39 +0800

    update

--- models.py
+++ models.py
@@ -55,17 +55,15 @@ class FewShotSegmentation(tf.keras.Model):
 
     # query.shape = (qn, h, w, 3)
     # support.shape = (nshot, h, w, 3)
-    # fg.shape = (nshot, h, w, nway)
-    # bg.shape = (nshot, h, w, nway = 1)
+    # labels.shape = (nshot, h, w, 1 + foreground number)
     # with_loss.shape = ()
-    query, support, fg, bg, with_loss = inputs;
-    assert bg.shape[-1] == 1; # because background can only has one mask
+    query, support, labels, with_loss = inputs;
     assert type(with_loss) is bool;
     imgs_concat = tf.keras.layers.Concatenate(axis = 0)([support, query]); # imgs_concat.shape = (nshot + qn, h, w, 3)
     img_fts = self.resnet101(imgs_concat); # img_fts.shape = (nshot + 1, nh, nw, 2048)
     supp_fts, qry_fts = tf.keras.layers.Lambda(lambda x: tf.split(x, (-1, query.shape[0]), axis = 0))(img_fts); # supp_fts.shape = (nshot, nh, nw, 2048), qry_fts.shape = (qn, nh, nw, 2048)
-    ds_fg = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], size = x[1].shape[1:3]))([fg, img_fts]); # ds_fg.shape = (nshot, nh, nw, nway)
-    ds_bg = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], size = x[1].shape[1:3]))([bg, img_fts]); # ds_bg.shape = (nshot, nh, nw, nway = 1)
+    ds_labels = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], size = x[1].shape[1:3]))([labels, img_fts]); # ds_labels.shape = (nshot, nh, nw, 1 + foreground number)
+    ds_bg, ds_fg = tf.keras.layers.Lambda(lambda x: tf.split(x, (1, -1), axis = -1))(ds_labels); # ds_bg.shape = (nshot, nh, nw, 1), ds_fg.shape = (nshot, nh, nw, foreground number)
     scores = list();
     bg_raw_score = ALPNet(qry_fts.shape[1], qry_fts.shape[2], qry_fts.shape[3], mode = 'gridconv', thresh = self.thresh)([qry_fts, supp_fts, ds_bg[..., 0]]); # bg_raw_score.shape = (qn, nh, nw)
     scores.append(bg_raw_score);
@@ -75,19 +73,23 @@ class FewShotSegmentation(tf.keras.Model):
         else ALPNet(qry_fts.shape[1], qry_fts.shape[2], qry_fts.shape[3], mode = 'mask', thresh = self.thresh)([qry_fts, supp_fts, ds_fg[..., i]]); # fg_raw_score.shape = (qn, nh, nw)
       scores.append(fg_raw_score);
     scores = tf.keras.layers.Lambda(lambda x: tf.stack(x, axis = -1))(scores); # scores.shape = (qn, nh, nw, 1 + foreground number)
-    pred = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], x[1].shape[1:3]))([scores, fg]); # us_scores.shape = (qn, h, w, 1 + foreground number)
+    pred = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], x[1].shape[1:3]))([scores, labels]); # us_scores.shape = (qn, h, w, 1 + foreground number)
     # get align_loss
     if with_loss:
       pred_cls = tf.keras.layers.Lambda(lambda x: tf.math.argmax(x, axis = -1))(pred); # pred_cls.shape = (qn, h, w)
       query_label = tf.one_hot(pred_cls, depth = pred.shape[-1], axis = -1); # pred_cls.shape = (qn, h, w, 1 + foreground number)
       query_bg, query_fg = tf.kersa.layers.Lambda(lambda x: tf.split(x, (1, -1), axis = -1))(query_label); # query_bg.shape = (qn, h, w, 1), query_fg.shape = (qn, h, w, foreground number)
+      scores = list();
       bg_raw_score = ALPNet(supp_fts.shape[1], supp_fts.shape[2], supp_fts.shape[3], mode = 'gridconv', thresh = self.thresh)([supp_fts, qry_fts, query_bg[..., 0]]); # bg_raw_score.shape = (nshot, nh, nw)
+      scores.append(bg_raw_score);
       for i in range(query_fg.shape[-1]):
         maxval = tf.keras.layers.Lambda(lambda x: tf.math.reduce_max(tf.nn.avg_pool2d(tf.expand_dims(x, axis = -1), (4, 4), strides = (1, 1), padding = 'VALID')))(query_fg[..., i]);
         fg_raw_score = ALPNet(supp_fts.shape[1], supp_fts.shape[2], supp_fts.shape[3], mode = 'gridconv+', thresh = self.thresh)([supp_fts, qry_fts, query_fg[..., i]]) if maxval > self.thresh \
           else ALPNet(supp_fts.shape[1], supp_fts.shape[2], supp_fts.shape[3], mode = 'mask', thresh = self.thresh)([supp_fts, qry_fts, query_fg[..., i]]); # fg_raw_score.shape = (nshot, nh, nw)
-      
-    return pred;
+        scores.append(fg_raw_score);
+      scores = tf.keras.layers.Lambda(lambda x: tf.stack(x, axis = -1))(scores); # scores.shape = (nshot, nh, nw, 1 + foreground number)
+      loss = tf.keras.losses.CategoricalCrossentropy(labels, scores);
+    return pred if with_loss == False else pred, loss;
 
 if __name__ == "__main__":
 

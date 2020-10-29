9cebcb1e21f157da8f666ba14c9432247b571087
root <root@LAPTOP-A2EG90UI.localdomain>
2020-10-29 23:07:55 +0800

    update

--- create_datasets.py
+++ create_datasets.py
@@ -7,11 +7,113 @@ from skimage.segmentation import felzenszwalb; # graph cut
 from skimage.measure import label; # label connected component
 import scipy.ndimage.morphology as snm; # morphology
 import SimpleITK as sitk;
+import niftiio as nio;
 import cv2;
 
 HIST_CUT_TOP = 0.5;
 NEW_SPA = [1.25, 1.25, 7.70]; # unified voxel spacing
 CROP_SIZE = [256, 256];
+
+# some helper functions
+def resample_by_res(mov_img_obj, new_spacing, interpolator = sitk.sitkLinear, logging = True):
+    resample = sitk.ResampleImageFilter()
+    resample.SetInterpolator(interpolator)
+    resample.SetOutputDirection(mov_img_obj.GetDirection())
+    resample.SetOutputOrigin(mov_img_obj.GetOrigin())
+    mov_spacing = mov_img_obj.GetSpacing()
+
+    resample.SetOutputSpacing(new_spacing)
+    RES_COE = np.array(mov_spacing) * 1.0 / np.array(new_spacing)
+    new_size = np.array(mov_img_obj.GetSize()) *  RES_COE 
+
+    resample.SetSize( [int(sz+1) for sz in new_size] )
+    if logging:
+        print("Spacing: {} -> {}".format(mov_spacing, new_spacing))
+        print("Size {} -> {}".format( mov_img_obj.GetSize(), new_size ))
+
+    return resample.Execute(mov_img_obj)
+
+def resample_lb_by_res(mov_lb_obj, new_spacing, interpolator = sitk.sitkLinear, ref_img = None, logging = True):
+    src_mat = sitk.GetArrayFromImage(mov_lb_obj)
+    lbvs = np.unique(src_mat)
+    if logging:
+        print("Label values: {}".format(lbvs))
+    for idx, lbv in enumerate(lbvs):
+        _src_curr_mat = np.float32(src_mat == lbv) 
+        _src_curr_obj = sitk.GetImageFromArray(_src_curr_mat)
+        _src_curr_obj.CopyInformation(mov_lb_obj)
+        _tar_curr_obj = resample_by_res( _src_curr_obj, new_spacing, interpolator, logging )
+        _tar_curr_mat = np.rint(sitk.GetArrayFromImage(_tar_curr_obj)) * lbv
+        if idx == 0:
+            out_vol = _tar_curr_mat
+        else:
+            out_vol[_tar_curr_mat == lbv] = lbv
+    out_obj = sitk.GetImageFromArray(out_vol)
+    out_obj.SetSpacing( _tar_curr_obj.GetSpacing() )
+    if ref_img != None:
+        out_obj.CopyInformation(ref_img)
+    return out_obj
+        
+def get_label_center(label):
+    nnz = np.sum(label > 1e-5)
+    return np.int32(np.rint(np.sum(np.nonzero(label), axis = 1) * 1.0 / nnz))
+
+def image_crop(ori_vol, crop_size, referece_ctr_idx, padval = 0., only_2d = True):
+    """ crop a 3d matrix given the index of the new volume on the original volume
+        Args:
+            refernce_ctr_idx: the center of the new volume on the original volume (in indices)
+            only_2d: only do cropping on first two dimensions
+    """
+    _expand_cropsize = [x + 1 for x in crop_size] # to deal with boundary case
+    if only_2d:
+        assert len(crop_size) == 2, "Actual len {}".format(len(crop_size))
+        assert len(referece_ctr_idx) == 2, "Actual len {}".format(len(referece_ctr_idx))
+        _expand_cropsize.append(ori_vol.shape[-1])
+        
+    image_patch = np.ones(tuple(_expand_cropsize)) * padval
+
+    half_size = tuple( [int(x * 1.0 / 2) for x in _expand_cropsize] )
+    _min_idx = [0,0,0]
+    _max_idx = list(ori_vol.shape)
+
+    # bias of actual cropped size to the beginning and the end of this volume
+    _bias_start = [0,0,0]
+    _bias_end = [0,0,0]
+
+    for dim,hsize in enumerate(half_size):
+        if dim == 2 and only_2d:
+            break
+
+        _bias_start[dim] = np.min([hsize, referece_ctr_idx[dim]])
+        _bias_end[dim] = np.min([hsize, ori_vol.shape[dim] - referece_ctr_idx[dim]])
+
+        _min_idx[dim] = referece_ctr_idx[dim] - _bias_start[dim]
+        _max_idx[dim] = referece_ctr_idx[dim] + _bias_end[dim]
+        
+    if only_2d:
+        image_patch[ half_size[0] - _bias_start[0]: half_size[0] +_bias_end[0], \
+                half_size[1] - _bias_start[1]: half_size[1] +_bias_end[1], ... ] = \
+                ori_vol[ referece_ctr_idx[0] - _bias_start[0]: referece_ctr_idx[0] +_bias_end[0], \
+                referece_ctr_idx[1] - _bias_start[1]: referece_ctr_idx[1] +_bias_end[1], ... ]
+
+        image_patch = image_patch[ 0: crop_size[0], 0: crop_size[1], : ]
+    # then goes back to original volume
+    else:
+        image_patch[ half_size[0] - _bias_start[0]: half_size[0] +_bias_end[0], \
+                half_size[1] - _bias_start[1]: half_size[1] +_bias_end[1], \
+                half_size[2] - _bias_start[2]: half_size[2] +_bias_end[2] ] = \
+                ori_vol[ referece_ctr_idx[0] - _bias_start[0]: referece_ctr_idx[0] +_bias_end[0], \
+                referece_ctr_idx[1] - _bias_start[1]: referece_ctr_idx[1] +_bias_end[1], \
+                referece_ctr_idx[2] - _bias_start[2]: referece_ctr_idx[2] +_bias_end[2] ]
+
+        image_patch = image_patch[ 0: crop_size[0], 0: crop_size[1], 0: crop_size[2] ]
+    return image_patch
+
+def copy_spacing_ori(src, dst):
+    dst.SetSpacing(src.GetSpacing())
+    dst.SetOrigin(src.GetOrigin())
+    dst.SetDirection(src.GetDirection())
+    return dst
 
 def process_CHAOST2(dataset_root):
 
@@ -27,56 +129,42 @@ def process_CHAOST2(dataset_root):
       if splitext(f)[1] == '.png':
         label = cv2.imread(join(dataset_root, sid, 'T2SPIR', 'Ground', f));
         if label is None:
-          print("label file %s is broken" % (join(dataset_root, sid, "T2SPIR", "Ground", labelfile)));
+          print("label file %s is broken" % (join(dataset_root, sid, "T2SPIR", "Ground", f)));
           continue;
-      labels = [t[1] for t in sorted(labels.items())];
-      labels = np.stack(labels, axis = 0);
+        idx = int(basename(f).split('-')[-1]);
+        labels[idx] = label;
+    labels = [t[1] for t in sorted(labels.items())];
+    labels = np.stack(labels, axis = 0); # labels.shape = (slice number, height, width)
+    labels = np.flip(labels, axis = 1);
+    for new_val, old_val in enumerate(sorted(np.unique(labels))):
+      labels[labels == old_val] = new_val;
+    labels = nio.np2itk(img = labels, ref_obj = slices);
     # filtering over bright area
     array = sitk.GetArrayFromImage(slices);
     hir = float(np.percentile(array, 100.0 - HIST_CUT_TOP));
     array[array > hir] = hir;
-    filtered_slices = sitk.GetImageFromArray(array);
-    filtered_slices.SetSpacing(slices.GetSpacing());
-    filtered_slices.SetOrigin(slices.GetOrigin());
-    filtered_slices.SetDirection(slices.GetDirection());
+    his_img_o = sitk.GetImageFromArray(array);
+    his_img_o = copy_spacing_ori(img_obj, his_img_o);
     # resampling the MR
-    resampler = sitk.ResampleImageFilter();
-    resampler.SetInterpolator(sitk.sitkLinear);
-    resampler.SetOutputDirection(filtered_slices.GetDirection());
-    resampler.SetOutputOrigin(filtered_slices.GetOrigin());
-    mov_spacing = filtered_slices.GetSpacing();
-    resampler.SetOutputSpacing(NEW_SPA);
-    RES_COE = np.array(mov_spacing) * 1.0 / np.array(new_spacing);
-    new_size = np.array(filtered_slices.GetSize()) * RES_COE;
-    resampler.SetSize([int(sz + 1) for sz in new_size]);
-    resampled_slices = resampler.Execute(filtered_slices);
+    img_spa_ori = img_obj.GetSpacing()
+    res_img_o = resample_by_res(his_img_o, [NEW_SPA[0], NEW_SPA[1], NEW_SPA[2]],
+                                interpolator = sitk.sitkLinear, logging = True);
+    # resampling the label
+    lb_arr = sitk.GetArrayFromImage(labels);
+    res_lb_o = resample_lb_by_res(labels,  [NEW_SPA[0], NEW_SPA[1], NEW_SPA[2] ], interpolator = sitk.sitkLinear,
+                                  ref_img = None, logging = True);
     # crop out rois
-    array = sitk.GetArrayFromImage(resampled_slices); # array.shape = (slice number, height, width)
-    array = np.transpose(array, (1,2,0)); # array.shape = (height, width, slice number)
-    expand_cropsize = [x + 1 for x in CROP_SIZE] + [array.shape[-1]]; # expand_cropsize = (256 + 1, 256 + 1, slice number)
-    image_patch = np.ones(expand_cropsize) * np.min(array);
-    half_size = tuple(np.array(expand_cropsize) // 2); # ((256 + 1) / 2, (256 + 1) / 2, slice_number / 2)
-    min_idx = [0, 0, 0];
-    max_idx = array.shape;
-    bias_start = [0, 0, 0];
-    bias_end = [0, 0, 0];
-    reference_ctr_idx = [array.shape[0] // 2, array.shape[1] // 2]; # (h / 2, w / 2)
-    for i in range(2):
-      bias_start[i] = np.min([half_size[i], reference_ctr_idx[i]]);
-      bias_end[i] = np.min([half_size[i], array.shape[i] - reference_ctr_idx[i]]);
-      min_idx[i] = reference_ctr_idx[i] - bias_start[i];
-      max_idx[i] = reference_ctr_idx[i] + bias_end[i];
-    image_patch[half_size[0] - bias_start[0]:half_size[0] + bias_end[0], \
-                half_size[1] - bias_start[1]:half_size[1] + bias_end[1], ...] = \
-    array[reference_ctr_idx[0] - bias_start[0]:reference_ctr_idx[0] + bias_end[0],
-          reference_ctr_idx[1] - bias_start[1]:reference_ctr_idx[1] + bias_end[1], ...];
-    image_patch = image_patch[0:CROP_SIZE[0], 0:CROP_SIZE[1],:];
-    array = np.transpose(image_patch, (2, 0, 1));
-    cropped_slices = sitk.GetImageFromArray(array);
-    cropped_slices.SetSpacing(filtered_slices.GetSpacing());
-    cropped_slices.SetOrigin(filtered_slices.GetOrigin());
-    cropped_slices.SetDirection(filtered_slices.GetDirection());
-    # TODO
+    res_img_a = sitk.GetArrayFromImage(res_img_o);
+    crop_img_a = image_crop(res_img_a.transpose(1,2,0), [256, 256],
+                            referece_ctr_idx = [res_img_a.shape[1] // 2, res_img_a.shape[2] //2],
+                            padval = res_img_a.min(), only_2d = True).transpose(2,0,1);
+    out_img_obj = copy_spacing_ori(res_img_o, sitk.GetImageFromArray(crop_img_a));
+    res_lb_a = sitk.GetArrayFromImage(res_lb_o);
+    crop_lb_a = image_crop(res_lb_a.transpose(1,2,0), [256, 256],
+                            referece_ctr_idx = [res_lb_a.shape[1] // 2, res_lb_a.shape[2] //2],
+                            padval = 0, only_2d = True).transpose(2,0,1);
+    out_lb_obj = copy_spacing_ori(res_img_o, sitk.GetImageFromArray(crop_lb_a));
+    # out_img_obj  out_lb_obj
 
 def process_SABS(dataset_root):
     

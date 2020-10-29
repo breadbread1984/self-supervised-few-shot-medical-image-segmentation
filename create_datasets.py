929524fce3e42fca39f5730acaa9fc30de878a35
breadbread1984 <breadbread1984@163.com>
2020-10-29 11:59:06 +0800

    update

--- create_datasets.py
+++ create_datasets.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
 
+from os import listdir;
+from os.path import join, splitext;
 import numpy as np;
 from skimage.segmentation import felzenszwalb; # graph cut
 from skimage.measure import label; # label connected component
 import scipy.ndimage.morphology as snm; # morphology
+import dicom;
+import simpleITK as sitk;
+import cv2;
 
-def gen_superpixel(img, thresh = 1e-4):
+def process_CHAOST2(dataset_root):
+
+  for sid in listdir(dataset_root):
+    # 1) process data
+    for f in listdir(join(dataset_root, sid, "T2SPIR", "DICOM_anon")):
+      if splitext(f)[1] == '.dcm':
+        slice = dicom.read_file(join(dataset_root, sid, "T2SPIR", "DICOM_anon", f));
+        # TODO
+    # 2) process labels
+    for f in listdir(join(dataset_root, sid, "T2SPIR", "Ground")):
+      if splitext(f)[1] == '.png':
+        label = cv2.imread(join(dataset_root, sid, "T2SPIR", "Ground", f));
+        if label is None:
+          print("label file %s is broken" % (join(dataset_root, sid, "T2SPIR", "Ground", f)));
+          continue;
+        # TODO
+
+def process_SABS(dataset_root):
+    
+  pass;
+
+def convert2foreground_segmentation(img, thresh = 1e-4):
 
   # img is the 3d image
   raw_seg = np.zeros_like(img);
@@ -54,3 +80,18 @@ def gen_superpixel(img, thresh = 1e-4):
   # fg_mask_vol: foreground mask with foreground 1, background 0
   # processed_seg_vol: foreground segmentation with label greater or equal 1, background 0
   return fg_mask_vol, processed_seg_vol;
+
+if __name__ == "__main__":
+    
+  from sys import argv;
+  if len(argv) != 3:
+    print("Usage: %s (CHAOST2|SABS) <dataset path>" % (argv[0]));
+    exit(1);
+  if argv[1] not in ['CHAOST2', 'SABS']:
+    print('unknown dataset!');
+    exit(1);
+  dataset_root = argv[2];
+  if argv[1] == 'CHAOST2':
+    process_CHAOST2(dataset_root);
+  elif argv[1] == 'SABS':
+    process_SABS(dataset_root);

4812e8ffad604da25784f29bed98c55ca79e5fc9
breadbread1984 <breadbread1984@163.com>
2020-11-20 02:16:05 +0000

    update

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import tensorflow as tf;
 from models import FewShotSegmentation;
 from create_datasets import parse_function_generator;
 
+physical_devices = tf.config.list_physical_devices('GPU');
+tf.config.experimental.set_memory_growth(physical_devices[0], True);
 batch_size = 1;
 
 def main():
